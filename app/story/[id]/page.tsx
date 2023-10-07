ee593365a1a4ae2d04a899c264f8671e529bf423
hanchayi <hanchayi@163.com>
2023-10-07 23:07:18 +0800

    detail style

--- app/story/[id]/page.tsx
+++ app/story/[id]/page.tsx
@@ -70,29 +70,32 @@ export default async function Story({ params }: { params: { id: string }} ) {
       <Tags marginTop='48px'/>
       <Tags marginTop='4px'/>
       <Categories />
+      <Contributors />
 
-      <div className='flex justify-between'>
-        <div>
-          <div>
-            <div></div>
-          </div>
-          <div>
-          +28 contributors
-          </div>
-        </div>
-
-        <div>
-          <div>2429</div>
-        </div>
+      <div className="mt-12 flex justify-center">
+        <button style={{
+          width: '260px',
+          height: '52px',
+          border: '2.5px solid #15097A',
+          borderRadius: '25px',
+        }}>Enter Dream</button>
       </div>
 
-      <div>
-        <button>Enter Dream</button>
+      <div className="mt-8 flex justify-center">
+        <div className='flex'>
+          <a>
+            View Dream Details
+          </a>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+            <mask id="mask0_0_6173" maskUnits="userSpaceOnUse" x="0" y="0" width="24" height="24">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0 24L0 0H24V24H0Z" fill="white"/>
+            </mask>
+            <g mask="url(#mask0_0_6173)">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M16 12L10 17V7L16 12V12Z" fill="#000989"/>
+            </g>
+          </svg>
+        </div>
       </div>
-
-      <a>
-      View Dream Details
-      </a>
     </main>
   </div>
 }
@@ -146,6 +149,75 @@ export function Categories() {
           }}
           key={c}>{c}</span>)
       }
+    </div>
+  )
+}
+
+function Contributors() {
+  return (
+    <div className='flex justify-between mt-6'>
+      <div className='flex items-center'>
+        <div className="relative"
+          style={{
+            width: '84px',
+            height: '42px',
+          }}
+        >
+          <div className="rounded-full absolute"
+            style={{
+              top: 0,
+              width: '42px',
+              height: '42px',
+              backgroundColor: '#C4C4C4',
+              border: '2px solid #969696'
+            }}
+          ></div>
+          <div className="rounded-full absolute"
+            style={{
+              top: 0,
+              left: '20px',
+              width: '42px',
+              height: '42px',
+              backgroundColor: '#C4C4C4',
+              border: '2px solid #969696'
+            }}
+          ></div>
+          <div className="rounded-full absolute"
+            style={{
+              top: 0,
+              left: '40px',
+              width: '42px',
+              height: '42px',
+              backgroundColor: '#C4C4C4',
+              border: '2px solid #969696'
+            }}
+          ></div>
+        </div>
+        <div style={{
+          color: '#000989',
+          fontSize: '14px',
+          fontStyle: 'normal',
+          fontWeight: '400',
+          lineHeight: '21px',
+        }}>
+        +28 contributors
+        </div>
+      </div>
+
+      <div className="flex items-center">
+        <Image
+          src={'/circle.svg'}
+          alt=""
+          width={18}
+          height={18}
+        ></Image>
+        <div style={{
+          color: '#15097A',
+          fontSize: '20px',
+          fontWeight: 700,
+          lineHeight: '30px',
+        }}>2429</div>
+      </div>
     </div>
   )
 }
